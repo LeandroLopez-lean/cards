ab86496ce1e56b07eeb744465b537933a55d8247
Leandro <leandrolopez131982@gmail.com>
2023-09-20 17:36:59 -0300

    proyecto cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Cards from './Cards/index.js';
+import './Cards/index.js';
+import './index.css';
+import {
+  faDownload,
+  faTimes,/* x */
+  faGear,/* config */
+  faWater, 
+  faBars,/* menu */
+  faExclamationCircle,
+  faSitemap,/* pc */
+} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const cardsData = [
+    { title: 'Nombre de la capa',
+    category: 'categoria',
+    options : [
+      {
+        icon: faDownload,
+        onClick: () => console.log('download')
+      },
+      {
+        icon: faExclamationCircle,
+        onClick: () => console.log('info')
+      },
+      {
+        icon: faBars,
+        onClick: () => console.log('menu')
+      },
+      {
+        icon: faGear,
+        onClick: () => console.log('config')
+      },
+    ]
+  },
+  { title: 'capacidad',
+    category: 'categoria superficie',
+    options : [
+      {
+        icon: faDownload,
+        onClick: () => console.log('download')
+      },
+     
+      { 
+        icon: faGear,
+        onClick: () => console.log('config')
+      }
+    ]
+  },
+  { title: 'capa lineal',
+    category: 'bajada'
+  },
+  { title: 'capa',
+    category: 'base'
+  },
+  { title: 'Nombre',
+    category: 'category'
+  }
+  ];  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Cards data={cardsData}/>
     </div>
   );
 }
-
 export default App;
